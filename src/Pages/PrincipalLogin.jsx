9f6f983438baa3f510222cf6239428f9849f4b31
srimathishanmugam8300 <ssrimathi8300@gmail.com>
2025-09-03 10:00:14 +0530

    d

--- src/Pages/PrincipalLogin.jsx
+++ src/Pages/PrincipalLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
+import  jwtDecode  from "jwt-decode";
 import PrincipalDashboard from './PrincipalDashboard';
 
 function PrincipalLogin() {
@@ -429,7 +429,7 @@ function PrincipalLogin() {
                 placeholder="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                disabled={isLoading}
+                disabled极={isLoading}
                 style={{ 
                   width: "100%", 
                   padding: "12px", 
